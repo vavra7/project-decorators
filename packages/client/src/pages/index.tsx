cd4c61baea746d74029ec90978347d2d289c6027
Tomáš Vavřinka <email@tomasvavrinka.cz>
2020-09-24 16:46:15 +0200

    react bind this decorator

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,7 +1,56 @@
-import { NextPage } from 'next';
+import { Component, ReactElement } from 'react';
 
-const Index: NextPage = () => {
-  return <div>index</div>;
-};
+interface State {
+  number: number;
+}
+
+function BindThis(): MethodDecorator {
+  return (target, propertyKey, descriptor) => {
+    if (!descriptor || typeof descriptor.value !== 'function') {
+      throw new TypeError(
+        `Only methods can be decorated with @BindThis(). <${propertyKey.toString()}> is not a method.`
+      );
+    }
+
+    return {
+      get(this) {
+        return (descriptor.value as any).bind(this);
+      }
+    };
+  };
+}
+
+class Index extends Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      number: 0
+    };
+  }
+
+  @BindThis()
+  private handleClick(): void {
+    console.log(this.state.number);
+  }
+
+  private handleClick2(): void {
+    console.log(this.state.number);
+  }
+
+  public render(): ReactElement {
+    return (
+      <div>
+        <button onClick={this.handleClick} type="button">
+          button
+        </button>
+
+        <button onClick={this.handleClick2} type="button">
+          button2
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Index;
